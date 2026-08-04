8283a425310414098b5f59a4a7e45db92f87db55
Justin Lopez <justinlopez588@gmail.com>
2026-08-04 16:43:49 -0500

    feat(mapstruct): ignore createdAt field in RoleEntity mapping

--- api/src/main/java/com/justinlopez/jobconnect/infrastructure/persistence/mapper/RoleMapper.java
+++ api/src/main/java/com/justinlopez/jobconnect/infrastructure/persistence/mapper/RoleMapper.java
@@ -3,6 +3,7 @@ package com.justinlopez.jobconnect.infrastructure.persistence.mapper;
 import com.justinlopez.jobconnect.domain.model.Role;
 import com.justinlopez.jobconnect.infrastructure.persistence.entity.RoleEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
@@ -10,6 +11,7 @@ public interface RoleMapper {
 
     Role toDomain(RoleEntity entity);
 
+    @Mapping(target = "createdAt", ignore = true)
     RoleEntity toEntity(Role role);
 
 }
